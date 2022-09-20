8050ec07d7cb1bebade21bc97ecdc7e39fc1b252
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2022-09-20 15:39:30 +0100

    fix: Resolve build issues

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://ab64233c9a90742109e1f9174fbffd80-58105548.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
